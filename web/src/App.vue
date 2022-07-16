d1e6ed028c2c0b2fdb774ee51a6e056c0b8d8ed4
muhfirdaus19 <firdaus.ex19@gmail.com>
2022-07-16 20:59:48 +0800

    clear app

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,40 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
 import Topbar from "./components/Topbar.vue";
 </script>
 
 <template>
   <div class="page">
-    <Navbar/>
-    <Topbar/>
+    <Navbar />
+    <Topbar />
     <RouterView />
   </div>
 </template>
 
-<!-- <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template> -->
-
-<style scoped>
-
-</style>
+<style scoped></style>
