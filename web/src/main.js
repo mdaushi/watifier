48e7f85dd85225f5e055bcd97e9620e108f46ac4
muhfirdaus19 <firdaus.ex19@gmail.com>
2022-07-17 00:22:34 +0800

    add sweet alert

--- web/src/main.js
+++ web/src/main.js
@@ -6,8 +6,12 @@ import router from "./router";
 import "@tabler/core/dist/js/tabler.min.js";
 import "@tabler/core/dist/css/tabler.min.css";
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 const app = createApp(App);
 
 app.use(router);
+app.use(VueSweetalert2)
 
 app.mount("#app");
