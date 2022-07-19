5dcebe4ff5f19ee02907dc43b6021f5f06cdf1fb
muhfirdaus19 <firdaus.ex19@gmail.com>
2022-07-19 12:08:50 +0800

    serve vue with express

--- api/app.js
+++ api/app.js
@@ -4,6 +4,11 @@ import nodeCleanup from 'node-cleanup'
 import routes from './routes.js'
 import { init, cleanup } from './whatsapp.js'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
@@ -13,7 +18,13 @@ const port = parseInt(process.env.PORT ?? 8000)
 app.use(cors())
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 app.use(express.json({ limit: '10mb' }))
-app.use('/', routes)
+app.use(express.static(path.join(__dirname, '../web/dist')));
+
+app.use('/api', routes)
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../web/dist/index.html'));
+});
 
 const listenerCallback = () => {
     init()
