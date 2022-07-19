ef5795ecfa4b5ece40e847e65116568af11d973c
muhfirdaus19 <firdaus.ex19@gmail.com>
2022-07-19 12:07:56 +0800

    disable image not found

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -12,7 +12,7 @@
       <h1
         class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3"
       >
-        <a href=".">
+        <!-- <a href=".">
           <img
             src="./static/logo.svg"
             width="110"
@@ -20,7 +20,7 @@
             alt="Tabler"
             class="navbar-brand-image"
           />
-        </a>
+        </a> -->
       </h1>
       <div class="navbar-nav flex-row order-md-last">
         <div class="nav-item d-none d-md-flex me-3">
@@ -328,10 +328,10 @@
             data-bs-toggle="dropdown"
             aria-label="Open user menu"
           >
-            <span
+            <!-- <span
               class="avatar avatar-sm"
               style="background-image: url(./static/avatars/000m.jpg)"
-            ></span>
+            ></span> -->
             <div class="d-none d-xl-block ps-2">
               <div>Paweł Kuna</div>
               <div class="mt-1 small text-muted">UI Designer</div>
